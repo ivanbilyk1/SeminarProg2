638d6a77e7e62d7e8b9cdb4ea7d037c1fc026d8a
Ivan Bilyk <thinkingyyy@gmail.com>
2024-04-15 20:47:58 +0300

    Funkcia Pretin vytvorí priesečník dvoch štruktúr alebo dvoch polí. Cyklus prehľadá všetky možné čísla v rozsahu od 1 do 1000 a ak sa číslo nachádza v oboch KCM, nastaví hodnotu 1.

--- SeminarProg2/Source.cpp
+++ SeminarProg2/Source.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 typedef struct {
-	int pole[1001];
+	int pole[101];
 }KCM;
 
 void Inicializacia(KCM* kcm) {
-    for (int i = 0; i <= 1000; i++) {
+    for (int i = 0; i <= 100; i++) {
         kcm->pole[i] = 0;
     }
 }
 
 void ADD(KCM* kcm, int number) {
-    if (number > 0 && number <= 1000) {
+    if (number > 0 && number <= 100) {
         kcm->pole[number] = 1;
     }
 }
@@ -20,7 +20,7 @@ KCM Ziednotenia(KCM kcm1, KCM kcm2) {
     KCM result;
     Inicializacia(&result);
 
-    for (int i = 1; i <= 1000; i++) {
+    for (int i = 1; i <= 100; i++) {
         if (kcm1.pole[i] || kcm2.pole[i]) {
             result.pole[i] = 1;
         }
@@ -33,7 +33,7 @@ KCM Pretin(KCM kcm1, KCM kcm2) {
     KCM result;
     Inicializacia(&result);
 
-    for (int i = 1; i <= 1000; i++) {
+    for (int i = 1; i <= 100; i++) {
         if (kcm1.pole[i] && kcm2.pole[i]) {
             result.pole[i] = 1;
         }
@@ -44,5 +44,36 @@ KCM Pretin(KCM kcm1, KCM kcm2) {
 
 
 int main() {
+    KCM kcm1, kcm2, ziednotenia, pretin;
+    Inicializacia(&kcm1);
+    Inicializacia(&kcm2);
+
+    ADD(&kcm1, 1);
+    ADD(&kcm1, 2);
+    ADD(&kcm1, 3);
+
+    ADD(&kcm2, 3);
+    ADD(&kcm2, 4);
+    ADD(&kcm2, 5);
+
+    ziednotenia = Ziednotenia(kcm1, kcm2);
+    pretin = Pretin(kcm1, kcm2);
+
+    printf("Ziednotiena KCM: {");
+    for (int i = 1; i <= 100; i++) {
+        if (ziednotenia.pole[i] == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("}\n");
+
+    printf("Pretin KCM: {");
+    for (int i = 1; i <= 100; i++) {
+        if (pretin.pole[i] == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("}\n");
+
 	return 0;
 }
