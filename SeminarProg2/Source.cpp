8b638916cb2ac44fa2f565b4856cd6f8544fc579
Ivan Bilyk <thinkingyyy@gmail.com>
2024-04-15 20:34:23 +0300

    Deklarovanie štruktúry s názvom KCM, ktorá obsahuje pole polí. Prvky tohto poľa sa použijú na označenie prítomnosti (1) alebo neprítomnosti (0) čísel KCM.

--- SeminarProg2/Source.cpp
+++ SeminarProg2/Source.cpp
@@ -4,6 +4,18 @@ typedef struct {
 	int pole[1001];
 }KCM;
 
+void Inicializacia(KCM* kcm) {
+    for (int i = 0; i <= 1000; i++) {
+        kcm->pole[i] = 0;
+    }
+}
+
+void ADD(KCM* kcm, int number) {
+    if (number > 0 && number <= 1000) {
+        kcm->pole[number] = 1;
+    }
+}
+
 int main() {
 	return 0;
 }
