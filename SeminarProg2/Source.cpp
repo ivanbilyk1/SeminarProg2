f3a1637abd969ec59af309d821283b2f4ca5e9d4
Ivan Bilyk <thinkingyyy@gmail.com>
2024-06-01 11:00:03 +0300

    .

--- SeminarProg2/Source.cpp
+++ SeminarProg2/Source.cpp
@@ -3,60 +3,63 @@
 #include "time.h"
 
 typedef struct {
-	int pole[101];
+	int* pole;
+    int size;
 }KCM;
 
-void Inicializacia(KCM* kcm) {
-    for (int i = 0; i <= 100; i++) {
+void Inicializacia(KCM* kcm, int size) {
+    kcm->pole = (int*)malloc(size * sizeof(int));
+    kcm->size = size;
+    for (int i = 0; i < size; i++) {
         kcm->pole[i] = 0;
     }
 }
 
 void ADD(KCM* kcm, int number) {
-    if (number > 0 && number <= 100) {
+    if (number > 0 && number < kcm->size) {
         kcm->pole[number] = 1;
     }
 }
 
-KCM Ziednotenia(KCM kcm1, KCM kcm2) {
-    KCM result;
-    Inicializacia(&result);
+void Ziednotenia(KCM* kcm1, KCM *kcm2, KCM *result) {
+    Inicializacia(result, kcm1->size);
 
-    for (int i = 1; i <= 100; i++) {
-        if (kcm1.pole[i] || kcm2.pole[i]) {
-            result.pole[i] = 1;
+    for (int i = 1; i < kcm1->size; i++) {
+        if (kcm1->pole[i] || kcm2->pole[i]) {
+            result->pole[i] = 1;
         }
     }
-
-    return result;
 }
 
-KCM Pretin(KCM kcm1, KCM kcm2) {
-    KCM result;
-    Inicializacia(&result);
+void Pretin(KCM* kcm1, KCM* kcm2, KCM* result) {
+    Inicializacia(result, kcm1->size);
 
-    for (int i = 1; i <= 100; i++) {
-        if (kcm1.pole[i] && kcm2.pole[i]) {
-            result.pole[i] = 1;
+    for (int i = 1; i < kcm1->size; i++) {
+        if (kcm1->pole[i] && kcm2->pole[i]) {
+            result->pole[i] = 1;
         }
     }
-
-    return result;
 }
 
+void FreeKCM(KCM* kcm) {
+    free(kcm->pole);
+    kcm->pole = NULL;
+    kcm->size = 0;
+}
 
-int main() {
-    srand((unsigned int)time(NULL));
-    int n1 = rand() % (10 + 1);
-    int n2 = rand() % (10 + 1);
-    int n3 = rand() % (10 + 1);
-    int n4 = rand() % (10 + 1);
-    int n5 = rand() % (10 + 1);
-    int n6 = rand() % (10 + 1);
-    printf("%d %d %d %d %d %d\n", n1, n2, n3, n4, n5, n6);
+void print(int size) {
+    int n1 = rand() % ((size - 1) + 1);
+    int n2 = rand() % ((size - 1) + 1);
+    int n3 = rand() % ((size - 1) + 1);
+    int n4 = rand() % ((size - 1) + 1);
+    int n5 = rand() % ((size - 1) + 1);
+    int n6 = rand() % ((size - 1) + 1);
+    printf("%d %d %d    %d %d %d\n", n1, n2, n3, n4, n5, n6);
     KCM kcm1, kcm2, ziednotenia, pretin;
-    Inicializacia(&kcm1);
-    Inicializacia(&kcm2);
+    Inicializacia(&kcm1, size);
+    Inicializacia(&kcm2, size);
+    Inicializacia(&ziednotenia, size);
+    Inicializacia(&pretin, size);
 
     ADD(&kcm1, n1);
     ADD(&kcm1, n2);
@@ -66,11 +69,11 @@ int main() {
     ADD(&kcm2, n5);
     ADD(&kcm2, n6);
 
-    ziednotenia = Ziednotenia(kcm1, kcm2);
-    pretin = Pretin(kcm1, kcm2);
+    Ziednotenia(&kcm1, &kcm2, &ziednotenia);
+    Pretin(&kcm1, &kcm2, &pretin);
 
     printf("Ziednotiena KCM: {");
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i < size; i++) {
         if (ziednotenia.pole[i] == 1) {
             printf("%d ", i);
         }
@@ -78,12 +81,25 @@ int main() {
     printf("}\n");
 
     printf("Pretin KCM: {");
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i < size; i++) {
         if (pretin.pole[i] == 1) {
             printf("%d ", i);
         }
-    } 
+    }
     printf("}\n");
 
+    FreeKCM(&kcm1);
+    FreeKCM(&kcm2);
+    FreeKCM(&pretin);
+    FreeKCM(&ziednotenia);
+}
+
+int main() {
+    srand((unsigned int)time(NULL));
+    int size;
+    printf("zadajte hranicu: ");
+    scanf_s("%d", &size);
+
+    print(size);
 	return 0;
 }
